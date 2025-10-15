62e00f36c9534ffb6fb40ba1752e9842e89232f3
Daryl Buchanan <buchanan-d8@ulster.ac.uk>
2025-10-15 15:03:47 +0100

    Adding event listener to start button.

---
+++ src/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("start-btn").addEventListener("click", function () {
+        console.log("Start button was clicked!");
+    });
+});
